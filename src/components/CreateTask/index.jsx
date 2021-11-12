a1b667daad85ffdc522884c1e2c6d382cdbf9e7c
Prg-maker <df3296709@gmail.com>
2021-11-11 21:35:16 -0300

    add new config

--- src/components/CreateTask/index.jsx
+++ src/components/CreateTask/index.jsx
@@ -1,20 +1,46 @@
+import { useState } from 'react'
 import styles from './styles.module.scss'
 
+import {api} from '../../services/api'
+
 export function CreateTask(){
+
+  const [message , setMessage] = useState('')
+  const [title , setTitle] = useState('')
+
+ function handleCreateTask(event){
+  event.preventDefault()
+
+  
+  if(!message.trim()){
+    return
+  }
+
+  api.post('/createMessage' , {  message , title})
+  
+  setMessage('')
+  setTitle('')
+ }
+
   return(
     <div className={styles.createTaskContentWrapper}>
       <h2>Cria a sua tarefa</h2>
-      <form >
-        <input 
-          type="text" 
-          placeholder="título da sua tarefa"
+      <form  onSubmit={handleCreateTask}>
+       
+        <input
           className={styles.inputCreate} 
-        />
-        <textarea 
           name="message" 
           id="message" 
-          placeholder='digite..'
-        ></textarea>
+          placeholder='título da sua tarefa'
+          onChange={event => setMessage(event.target.value)}
+          value={message}
+        />
+         <textarea 
+          type="text" 
+          placeholder="tasks"
+          onChange={event => setTitle(event.target.value)}
+          value={title}
+        />
         <button className={styles.buttonCreate}>Criar</button>
       </form>
     </div>
--- src/components/ListMessage/index.jsx
+++ src/components/ListMessage/index.jsx
@@ -1,24 +1,79 @@
 import styles from './styles.module.scss'
+import {v4 as uuid}  from 'uuid'
+import {api} from '../../services/api'
+import { useEffect, useState } from 'react'
+
+
+import io from 'socket.io-client'
+
+const socket =  io('http://localhost:4000/')
+
+
+
+
 
 export function ListMessage(){
+
+
+  const [messages , setMessages] = useState([])
+  const messageQueue = []
+
+  useEffect(() => {
+    api.get('/listMessage').then(response => {
+      console.log(response.data)
+      setMessages(response.data)
+    })
+  } , [])
+
+  
+  useEffect(() => {
+    socket.on('new_message' , (newMessage) => {
+
+      messageQueue.push(newMessage)
+      if(messageQueue.length > 0){
+        setMessages(prevState => [
+          messageQueue[0],
+          ...prevState,
+
+        ]) 
+      }
+    })
+  },[])
+  
+
+ 
+  function handleDeleteMessage(id){
+    api.post('/deleteMessage' , {id})
+
+  }
+
   return(
     <div className={styles.listMessageContentWrapper}>
       <h2 className={styles.textCenter}>To do</h2>
 
       <div className={styles.listMessage}>
         <ul>
-          <li className={styles.message}>
-            <button className={styles.button} >delete</button>
-            <h4 className={styles.title}>title</h4>
-            <p className={styles.text}>texto</p>
-          </li>
 
-          <li className={styles.message}>
-            <button className={styles.button} >delete</button>
-            <h4 className={styles.title}>title</h4>
-            <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio dolorem eligendi animi eaque ipsa nesciunt enim ratione quod unde asperiores! Unde esse totam repellendus necessitatibus quibusdam consequatur velit maiores animi.</p>
-          </li>
+          {messages.map(message=> {
+            return(
+              <li key={message.id} className={styles.message}>
 
+                <a href={message.id}>
+                  <button 
+                    onClick={() => handleDeleteMessage(message.id)} 
+                    className={styles.button}>delete
+                  </button>
+                </a>
+
+                <h4 className={styles.title}>{message.title}</h4>
+                <p className={styles.text}>{message.message}</p>
+              </li>
+            )
+          })}
+
+      
+
+      
 
         
        
